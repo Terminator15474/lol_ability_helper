3207d8606da95ebb8180854ebd1113e405ba402e
Terminator15474 <jandommert8@gmail.com>
2024-02-06 08:35:44 +0100

    added champion endpoint

--- backend/main.go
+++ backend/main.go
@@ -12,19 +12,19 @@ import (
 )
 
 func main() {
-	data := network.GetChampions()
-	abilities := []string{}
-	for _, champ := range data.Data {
-		abilities = append(abilities, network.GetAbilities(champ.Id)...)
+	champions := network.GetChampions()
+	championNames := []string{}
+	for _, name := range champions.Data {
+		championNames = append(championNames, name.Name)
+	}
+
+	abilityNames := []string{}
+	for _, champ := range champions.Data {
+		abilityNames = append(abilityNames, network.GetAbilities(champ.Id)...)
 	}
 
 	http.HandleFunc("/abilities", func(res http.ResponseWriter, req *http.Request) {
-		// set headers
-		res.Header().Set("Access-Control-Allow-Origin", "*")
-		res.Header().Set("Content-Type", "text/html")
-		res.Header().Set("Access-Control-Allow-Methods", "*")
-		res.Header().Set("Access-Control-Allow-Credentials", "true")
-		res.Header().Set("Access-Control-Allow-Headers", "*")
+		setHeaders(res)
 
 		start := req.URL.Query().Get("start")
 		end := req.URL.Query().Get("end")
@@ -44,7 +44,7 @@ func main() {
 		}
 
 		var filtered = []string{}
-		for _, elem := range abilities {
+		for _, elem := range abilityNames {
 			replaced := strings.NewReplacer("!", "", "?", "", " ", "").Replace(strings.ToLower(elem))
 			if length != -1 && len(replaced) != int(length) {
 				continue
@@ -57,6 +57,43 @@ func main() {
 		slices.Sort(filtered)
 		templates.Abilities(filtered).Render(req.Context(), res)
 	})
+
+	http.HandleFunc("/champions", func(res http.ResponseWriter, req *http.Request) {
+		setHeaders(res)
+
+		start := req.URL.Query().Get("start")
+		end := req.URL.Query().Get("end")
+		contains := req.URL.Query().Get("contains")
+		not := req.URL.Query().Get("not")
+		len_str := req.URL.Query().Get("length")
+
+		var length int64
+		if len(len_str) > 0 {
+			temp, err := strconv.ParseInt(len_str, 10, 64)
+			if err != nil {
+				fmt.Printf("Error: %s", err)
+				return
+			}
+			length = temp
+		} else {
+			length = -1
+		}
+
+		var filtered = []string{}
+		for _, elem := range championNames {
+			replaced := strings.NewReplacer("!", "", "?", "", " ", "").Replace(strings.ToLower(elem))
+			if length != -1 && len(replaced) != int(length) {
+				continue
+			}
+
+			if filter(replaced, start, contains, end, not) {
+				filtered = append(filtered, elem)
+			}
+		}
+		slices.Sort(filtered)
+		templates.Abilities(filtered).Render(req.Context(), res)
+	})
+
 	fmt.Println("Listening on :8080")
 	http.ListenAndServe("localhost:8080", nil)
 }
@@ -72,4 +109,12 @@ func filter(s string, start string, contains string, end string, not string) boo
 		ret = ret && !strings.ContainsAny(s, not)
 	}
 	return ret
+}
+
+func setHeaders(res http.ResponseWriter) {
+	res.Header().Set("Access-Control-Allow-Origin", "*")
+	res.Header().Set("Content-Type", "text/html")
+	res.Header().Set("Access-Control-Allow-Methods", "*")
+	res.Header().Set("Access-Control-Allow-Credentials", "true")
+	res.Header().Set("Access-Control-Allow-Headers", "*")
 }
